5182d3fba53d4ad64980308830796e1ceba1caf1
jpbulman <jpbulman@wpi.edu>
2018-09-25 00:46:29 -0400

    Problem 18 done

--- src/PY/12.py
+++ src/PY/12.py
@@ -2,23 +2,29 @@
 def triangle(n):
     return (n*(n+1))//2
 
-numberOfDivisors = [1]*999999999
+numberOfDivisors = {
+    2:0,
+    3:0,
+    4:1
+}
 
 #Returns the number of divisors a number has
 def numDivs(n):
-    if(n==1):
-        return 1
-    elif(numberOfDivisors[n]!=1):
-        return numberOfDivisors[n]
-    else:
-        for i in range(2,(n//2)+1):
+    if numberOfDivisors.get(n) is None:
+        start = n//2
+        for i in range(start,1,-1):
             if(n%i==0):
-                return numDivs(n//i)-1+(numDivs(i)-1)
+                num = numDivs(start)
+                numberOfDivisors[n] = 1+num
+                return 1+1+1+num
+    else:
+        return 1+numberOfDivisors[n]
 
-currBest = 0
-for i in range(1,900):
-    divs = numDivs(triangle(i))
-    if(divs>=currBest):
-        currBest=divs
+# currBest = 0
+# for i in range(1,800):
+#     divs = numDivs(triangle(i))
+#     #print(str(divs)+" "+str(triangle(i)))
+#     if(divs>=currBest):
+#         currBest=divs
 
-print(currBest)
+print(numDivs(7))
